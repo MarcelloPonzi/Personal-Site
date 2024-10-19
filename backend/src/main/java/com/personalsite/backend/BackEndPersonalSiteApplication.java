011e7e459609962329fa0c275ea01930ea227c63
MarcelloPonzi <91527254+MarcelloPonzi@users.noreply.github.com>
2024-10-19 15:08:22 +0200

    Added backend project folder

---
+++ backend/src/main/java/com/personalsite/backend/BackEndPersonalSiteApplication.java
@@ -0,0 +1,13 @@
+package com.personalsite.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndPersonalSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndPersonalSiteApplication.class, args);
+	}
+
+}
